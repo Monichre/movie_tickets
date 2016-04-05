2e2218edd98ab064af507af8d805ec3c4b22cde4
liam ellis and nick arrasate <nick@>
2016-04-05 16:41:44 -0700

    everything

---
+++ js/scripts.js
@@ -0,0 +1,47 @@
+function Movie(title, age, time) {
+  this.name = title;
+  this.age = age;
+  this.time = time;
+}
+
+Movie.prototype.addPrice = function(){
+  if (this.time < 6 && (this.name == 'birdman' || this.name == 'drive')){
+    var price = 5;
+    return price;
+  } else if (this.time < 6 && (this.name == 'birds' || this.name == 'starwars')){
+    var price = 3;
+    return price;
+  } else if (this.time > 6 && (this.name == 'birdman' || this.name == 'drive')){
+    var price = 10;
+    return price;
+  } else {
+    var price = 7;
+    return price;
+  };
+}
+
+  //*************************************** UI FUNCT *********************************
+
+
+
+  $(document).ready(function() {
+    var ticket = [];
+    var total = 0;
+    $('#movies').submit(function(event){
+      event.preventDefault();
+      var age = $('#Age').val();
+      var movie = $(this).find('.radio :checked').val();
+      var time = $("." + movie).val();
+
+      var movieInfo = new Movie(movie, age, time);
+
+      ticket.push(movieInfo.addPrice());
+      console.log(ticket);
+
+      // ticket.forEach(function(ticket) {
+      //   return total += ticket;
+      // });
+
+
+    });
+  });
